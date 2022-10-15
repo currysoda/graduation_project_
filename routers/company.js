b415652ddc7b1ee58f967a57d0ca69008c0db7a0
currysoda <currysoda1598@gmail.com>
2022-10-16 02:10:32 +0900

    Update company.js

--- routers/company.js
+++ routers/company.js
@@ -20,8 +20,6 @@ module.exports = (app) => {
     router.get('/staff_list',(req,res) => {
         // console.log(request.user);
 
-        
-        
         res.render(`staff_list.pug`);
     });
 
