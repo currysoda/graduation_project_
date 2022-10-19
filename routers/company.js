cecc62ff1d4d29fb32dd3c860dbe0eb354e3663d
currysoda <currysoda1598@gmail.com>
2022-10-19 14:22:05 +0900

    10/18

--- routers/company.js
+++ routers/company.js
@@ -1,42 +1,61 @@
 var express = require(`express`);
 var router = require(`express`).Router();
+var db = require(`../lib/db`);
+
 
 module.exports = (app) => {
 
-    var logout = require(`./logout`) ();
+    var logout = require(`./logout`)();
     router.use(`/logout`, logout);
 
-    router.get('/',(req,res) => {
+    router.get('/', (req, res) => {
         // console.log(request.user);
-        if(req.isAuthenticated()) {
+        if (req.isAuthenticated()) {
 
             res.render(`company.pug`);
 
-        } else { 
+        } else {
             res.send(`please login`);
         }
     });
 
-    router.get('/create_company',(req,res) => {
+    router.get('/create_company', (req, res) => {
         // console.log(request.user);
         res.render(`create_company.pug`);
     });
 
-    router.post('/create_company/confirm',(req,res) => {
+    router.post('/create_company/confirm', (req, res) => {
         // console.log(request.user);
 
-        console.log(req.body.company_name);
-        
+        if (req.isAuthenticated()) {
+
+            console.log(req.body.company_name);
+
+            async function register_company() {
+                let sql = ` INSERT INTO company_list()
+                            VALUES(NULL,?);`;
+                let values = [req.body.company_name];
+
+                const connection = await db();
+                const [rows, fields] = await connection.execute(sql, values);
+
+                console.log(rows);
+            }
+            register_company();
+
+            res.send(req.body.company_name);
+
+        } else {
+            res.send(`please login`);
+        }
     });
 
-    router.get('/staff_list',(req,res) => {
+    router.get('/staff_list', (req, res) => {
         // console.log(request.user);
-        if(req.isAuthenticated()) {
-            
-
+        if (req.isAuthenticated()) {
 
             res.render(`staff_list.pug`);
-        } else { 
+        } else {
             res.send(`please login`);
         }
     });
