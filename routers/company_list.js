8a474960d58d62db1ebcaccb36ebf738332815d6
currysoda <98086265+currysoda@users.noreply.github.com>
2022-11-19 01:41:21 +0900

    11/18 -ing
    create_company.js 제작중

--- routers/company_list.js
+++ routers/company_list.js
@@ -6,9 +6,26 @@ module.exports = () => {
 
     router.get('/', (req, res) => {
 
-        // -ing
+        // req.session.passport.user.id === email 로 유저 식별 
+        let user_email = req.session.passport.user.id;
+        let user_id;
 
-        res.send("good");
+        async function find_user_id() {
+            let sql = `SELECT userID FROM user WHERE email=?;`;
+            let values = [user_email];
+
+            const connection = await db();
+            const [rows, fields] = await connection.execute(sql, values);
+
+            user_id = rows[0].userID;
+
+            // console.log(typeof(rows));
+            // console.log(`results : ${inspect(rows[0].userID)}`);
+
+            await connection.end();
+        }
+
+        res.send(`hi`);
     });
 
     return router;
--- routers/create_company.js
+++ routers/create_company.js
@@ -18,6 +18,7 @@ module.exports = () => {
         let comapny_name = req.body.company_name;
         let chief_email = req.session.passport.user.id;
         let chief_user_id;
+        let already_exist = { bool: false };
 
         async function find_user_id() {
             let sql = `SELECT userID FROM user WHERE email=?;`;
@@ -36,8 +37,28 @@ module.exports = () => {
 
         // find_user_id();
 
-        async function registed_company() {
+        async function duplicate_check() {
+            let sql = `SELECT * FROM company_list WHERE company_name=? AND company_chief_userID=?`;
+            let values = [comapny_name, Number(chief_user_id)];
 
+            const connection = await db();
+
+            const [rows, fields] = await connection.execute(sql, values);
+
+            // console.log(inspect(rows));
+
+            if (Object.entries(rows).length === 0) {
+                // console.log("0");
+            } else {
+                // console.log("1");
+                // console.log(already_exist);
+                already_exist.bool = true;
+                // console.log(already_exist);
+            }
+        }
+
+        async function registed_company() {
+            // create company
             let sql = `INSERT INTO company_list(companyID, company_name, company_chief_userID) VALUES(NULL, ?, ?);`;
             let values = [comapny_name, Number(chief_user_id)];
 
@@ -48,6 +69,12 @@ module.exports = () => {
             // console.log(typeof(rows));
             // console.log(`results : ${inspect(rows)}`);
 
+            // create default team
+
+            // create permission
+
+            // create user_rel_company_list
+
             await connection.end();
         }
 
@@ -56,11 +83,19 @@ module.exports = () => {
         try{
             async function execution_order() {
                 await find_user_id();
-                await registed_company();
+                await duplicate_check();
+                // console.log(already_exist.bool);
+                // console.log(typeof(already_exist.bool));
+                if(already_exist.bool === true) {
+                    res.status(200).render(`already_exist_company`);
+                }
+                else {
+                    await registed_company();
+                    res.status(201).render(`create_company_confirm`);
+                }
             }
-            execution_order();
 
-            res.status(201).render(`create_company_confirm`);
+            execution_order();
 
         } catch(e) {
             res.status(400).render(`create_company_failure`);
