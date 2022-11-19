fd9025b661a291c7ec97fe7e206c25689d002956
currysoda <98086265+currysoda@users.noreply.github.com>
2022-11-20 01:26:26 +0900

    11/19 -ing

--- routers/company_list.js
+++ routers/company_list.js
@@ -1,6 +1,7 @@
 var express = require(`express`);
 var router = require(`express`).Router();
 var db = require(`../lib/db`);
+var inspect = require('object-inspect');
 
 module.exports = () => {
 
@@ -9,6 +10,7 @@ module.exports = () => {
         // req.session.passport.user.id === email 로 유저 식별 
         let user_email = req.session.passport.user.id;
         let user_id;
+        let my_company_list_id = [];
 
         async function find_user_id() {
             let sql = `SELECT userID FROM user WHERE email=?;`;
@@ -17,13 +19,43 @@ module.exports = () => {
             const connection = await db();
             const [rows, fields] = await connection.execute(sql, values);
 
-            user_id = rows[0].userID;
+            user_id = await Number(rows[0].userID);
 
             // console.log(typeof(rows));
             // console.log(`results : ${inspect(rows[0].userID)}`);
 
             await connection.end();
         }
+
+        async function my_company_list() {
+            let sql = `SELECT companyID FROM user_rel_company_list WHERE userID = ?;`;
+            let values = [user_id];
+
+            const connection = await db();
+            const [rows, fields] = await connection.execute(sql, values);
+
+            console.log(typeof(rows));
+            console.log(`results : ${inspect(rows)}`);
+
+            await rows.forEach((item, index, array) => {
+                console.log(typeof(item.companyID));
+                console.log(item.companyID);
+                my_company_list_id.push(item.companyID);
+            });
+
+            console.log(my_company_list_id);
+
+            await connection.end();
+        }
+
+        async function execution_order()
+        {
+            await find_user_id();
+            await my_company_list();
+        }
+
+        execution_order();
+        
 
         res.send(`hi`);
     });
